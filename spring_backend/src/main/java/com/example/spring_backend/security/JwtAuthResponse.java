d3e067f52007efd5520df29be37531a5352e8216
Neel Mehta <neelmehta0086@gmail.com>
2023-06-16 20:19:49 +0530

    feat:Jwt setup

---
+++ spring_backend/src/main/java/com/example/spring_backend/security/JwtAuthResponse.java
@@ -0,0 +1,8 @@
+package com.example.spring_backend.security;
+
+import lombok.Data;
+
+@Data
+public class JwtAuthResponse {
+    private String token;
+}
---
+++ spring_backend/src/main/java/com/example/spring_backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.example.spring_backend.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtTokenHelper jwtTokenHelper;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestToken = request.getHeader("Authorization");
+        String username=null;
+        String token=null;
+        if(request!=null && requestToken.startsWith("Bearer")){
+            token = requestToken.substring(7);
+            try {
+                username = this.jwtTokenHelper.getUsernameFromToken(token);
+            }
+            catch(IllegalArgumentException e){
+                System.out.println("Unable to get Jwt token");
+            }
+            catch(ExpiredJwtException e){
+                System.out.println("Token has expired");
+            }
+            catch(MalformedJwtException e){
+                System.out.println("Invlaid Jwt token");
+            }
+        }
+        else{
+            System.out.println("Error doesn;t begin with bearer");
+        }
+
+        if(username!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if(this.jwtTokenHelper.validateToken(token,null)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(null);
+            }
+        }
+        else{
+            System.out.println("Username is null");
+        }
+        filterChain.doFilter(request,response);
+
+    }
+}
